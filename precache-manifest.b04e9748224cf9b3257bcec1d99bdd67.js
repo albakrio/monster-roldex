5fbdd4b8e0468f81ca0206aef7c4bd10d1959199
omar-albakri <albakrio@outlook.com>
2020-04-24 16:27:28 -0700

    Updates

---
+++ precache-manifest.b04e9748224cf9b3257bcec1d99bdd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fde50ec6d62f365f6c3f72fe202bfc",
+    "url": "/monster-roldex/index.html"
+  },
+  {
+    "revision": "0fa39adbeee82d8387ad",
+    "url": "/monster-roldex/static/css/main.991ab5bb.chunk.css"
+  },
+  {
+    "revision": "2e92dffff18a4623c83b",
+    "url": "/monster-roldex/static/js/2.0da73388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roldex/static/js/2.0da73388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa39adbeee82d8387ad",
+    "url": "/monster-roldex/static/js/main.6e25c3b4.chunk.js"
+  },
+  {
+    "revision": "dc422dbb2b93af6afa4b",
+    "url": "/monster-roldex/static/js/runtime-main.ca9fb07a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-roldex/static/media/logo.5d5d9eef.svg"
+  }
+]);
